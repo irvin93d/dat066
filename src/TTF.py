05223be7732059e4c40cd7e49677cd40ab1a632e
Duane Irvin <irvin93d@gmail.com>
2017-11-16 15:33:39 +0100

    Implement TTF

---
+++ src/TTF.py
@@ -0,0 +1,22 @@
+"""pyttsx3 is the engine for TTF"""
+import pyttsx3
+
+class TTF():
+    """Text-to-Speech
+
+    Use pyttsx3 to play a voice from a text string
+
+        :Example:
+        ttf = TTF()
+        ttf.say("Hey there!")
+
+    """
+    def __init__(self):
+        self.engine = pyttsx3.init()
+        """ TODO set volume and language """
+
+    def say(self, text):
+        """play string text from speakers"""
+        # TODO Test that text is a string
+        self.engine.say(text)
+        self.engine.runAndWait()
