9933e9313bd08c5ce4733b9d6eef18ac4f7086a1
Jesper Holm <holmje@student.chalmers.se>
2017-11-20 16:23:04 +0100

    Compability for Linux fixed
    More functions added, load picture, snake case added,

--- src/GUItest.py
+++ src/GUItest.py
@@ -5,61 +5,76 @@ from PIL import Image
 from PIL import ImageTk
 import cv2
 
+global picture_file_path
+picture_file_path = 'face.jpg'
+
 def __init__():
-    createwindow(800, 600)
+    create_window(800, 600)
 
 
-def createwindow(xSize, ySize):
+def create_window(x_length_window, y_length_window):
     """Creates a Window with the size as input (x size, y size)
        createwindow(200,200) will a create a window 200x200px
     """
 
+    global window
     window = tk.Tk()
     window.title("GUI Title V.1.0")
-    screen = getscreenresolution()
-    xPos = str(int(screen[0] / 2 - (xSize / 2)))
-    yPos = str(int(screen[1] / 2 - (ySize / 2)))
-    window.geometry("800x600+" + xPos + "+" + yPos)
+    screen_size = get_screen_resolution()
+
+    x_window_position = str(int(screen_size[0] / 2 - (x_length_window / 2)))
+    y_window_position = str(int(screen_size[1] / 2 - (y_length_window / 2)))
+    window.geometry("800x600+" + x_window_position + "+" + y_window_position)
 
     menubar = Menu(window)
     options = Menu(menubar, tearoff=0)
-    helpmenu = Menu(menubar, tearoff=0)
+    help_menu = Menu(menubar, tearoff=0)
     menubar.add_cascade(label="Options", menu=options)
     options.add_command(label="Exit", command=window.quit)
-    menubar.add_cascade(label="Help", menu=helpmenu)
-    helpmenu.add_command(label="Information", command=window.quit)
+    menubar.add_cascade(label="Help", menu=help_menu)
+    help_menu.add_command(label="Information", command=window.quit)
     window.config(menu=menubar)
 
-    viewframe = Frame(window, bg='black', width=xSize, height=ySize - 60)
-    path = 'face.jpg'
-    image = cv2.imread(path)
-    picturepanel = Label(viewframe)
-    picturepanel.image = image
-    picturepanel.pack()
-    viewframe.pack(side=TOP)
+    #viewframe = Frame(window, bg='black', width=xSize, height=ySize - 60)
+    #image = ImageTk.PhotoImage(Image.open(path))
+    img = load_image(picture_file_path);
+    picture_label = Label(window, image = img)
+    #view_frame.pack(side=TOP)
+    picture_label.pack(side="top")
 
-    buttonframe = Frame(window, width=xSize, height=int(float(yPos) * 0.2), bg='white')
-    buttonframe.pack(side=BOTTOM)
+    button_frame = Frame(window, width=x_length_window, height=int(float(y_window_position) * 0.2), bg='white')
+    button_frame.pack(side=BOTTOM)
 
-    camerabutton = Button(buttonframe, text='Capture', fg="white", bg='red', width=15, height=3
-                          , command=lambda: captureimage())
-    camerabutton.pack()
+    camera_button = Button(button_frame, text='Capture', fg="white", bg='red', width=15, height=3
+                          , command=lambda: capture_image())
+    camera_button.pack()
 
     tk.mainloop()
 
 
-def getscreenresolution():
+def get_screen_resolution():
     """Returns the screen resolution of the used device
     """
-    user32 = ctypes.windll.user32
-    screensize = user32.GetSystemMetrics(0), user32.GetSystemMetrics(1)
-    return screensize
+    screen_size = window.winfo_screenwidth(), window.winfo_screenheight()
+    return screen_size
 
 
-def captureimage():
+def capture_image():
     """Captures an image
     """
     print("Capture Image invoked")
 
+
+def scale_image(original_image):
+    pass
+
+
+def load_image(filename):
+    """Loads an image and returns it, it takes
+    file path as input"""
+
+    path = filename
+    img = ImageTk.PhotoImage(Image.open(path))
+    return img
 
 __init__()
