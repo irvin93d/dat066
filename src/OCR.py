e7fc97c7cabc13ba891dd313645d71863ec15314
Duane Irvin <irvin93d@gmail.com>
2017-12-04 10:05:08 +0100

    Replace multiple space with one space

--- src/OCR.py
+++ src/OCR.py
@@ -2,10 +2,19 @@
     Currently just a wrapper for pytesseract.
 """
 import pytesseract
+import re
 
 def image_to_string(image):
     """ Return string of text from given image
     """
     text = pytesseract.image_to_string(image)
+
+    # Remove Newline
     text = text.replace('\n', ' ')
+    # Remove multiple space
+    text = re.sub('[ \t]+', ' ', text)
+
+
+    
+
     return text
