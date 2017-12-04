3acc7cf9ebd99fbd438f075082e5b3fc977bfd4f
Duane Irvin <irvin93d@gmail.com>
2017-12-04 11:01:54 +0100

    Fix bug, replace newline with space

--- src/OCR.py
+++ src/OCR.py
@@ -10,7 +10,7 @@ def image_to_string(image):
     text = pytesseract.image_to_string(image)
 
     # Remove single newlines (always removes one newline in a group)
-    text = re.sub('\n(?!\n)', '', text)
+    text = re.sub('\n(?!\n)', ' ', text)
     # Remove multiple Newline
     text = re.sub('\n+', '\n', text)
     # Remove multiple space
