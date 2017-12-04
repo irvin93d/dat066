4e60e5fe006588705b6486d68f8f7eb1419a5ce4
Duane Irvin <irvin93d@gmail.com>
2017-12-04 09:54:13 +0100

    Remove newline from TTS

--- src/OCR.py
+++ src/OCR.py
@@ -7,4 +7,5 @@ def image_to_string(image):
     """ Return string of text from given image
     """
     text = pytesseract.image_to_string(image)
+    text = text.replace('\n', ' ')
     return text
