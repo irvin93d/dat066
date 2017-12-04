72845586a3ca496aea9f934db51465b5317fade1
Duane Irvin <irvin93d@gmail.com>
2017-12-04 10:36:12 +0100

    Remove only single newline

--- src/OCR.py
+++ src/OCR.py
@@ -1,16 +1,18 @@
 """ Recognize characters from image.
     Currently just a wrapper for pytesseract.
 """
-import pytesseract
 import re
+import pytesseract
 
 def image_to_string(image):
     """ Return string of text from given image
     """
     text = pytesseract.image_to_string(image)
 
-    # Remove Newline
-    text = text.replace('\n', ' ')
+    # Remove single newlines (always removes one newline in a group)
+    text = re.sub('\n(?!\n)', '', text)
+    # Remove multiple Newline
+    text = re.sub('\n+', '\n', text)
     # Remove multiple space
     text = re.sub('[ \t]+', ' ', text)
 
